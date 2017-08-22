2c45bb29b93e4ea774123d2fe88c2301b5607eb6
Alexey <zurgul@gmail.com>
2017-08-22 13:10:32 +0300

    support two db types, error if no files on s3, columns order

--- csv2db.sh
+++ csv2db.sh
@@ -1,32 +1,41 @@
 #!/bin/bash
 
-if [ "$#" -ne 12 ]; then
+if [ "$#" -ne 14 ]; then
   echo -e "Err: Illegal number of parameters\n"
   echo "Options
-   -s source s3 path to folder with csv files
-   -h  db host url
-   -P  db port
-   -u  db user name
-   -p  db pass
-   -n  db name
+   -t        db type mysql or pg
+   -s        source s3 path to folder with csv files
+   -h        db host url
+   -P        db port
+   -u        db user name
+   -p        db pass
+   -n        db name
 
-ex: csv2pg.sh -s s3://dm-test-storage/tables -h localhost -P 5432 -u postgres -p proov123 -n proov_db
+ex: csv2db.sh -t pg -s s3://dm-test-storage/tables -h localhost -P 5432 -u postgres -p proov123 -n proov_db
 "
   exit 1
 fi
 
-while getopts s:h:P:u:p:n: option
+while getopts t:s:h:P:u:p:n: option
 do
-# echo option ${option} = $OPTARG
+#  echo ${option} ${OPTARG}
  case "${option}" in
+ t) DBTYPE=${OPTARG};;
  s) S3URL=${OPTARG};;
  h) HOST=${OPTARG};;
  P) PORT=${OPTARG};;
- u) USER=$OPTARG;;
- p) PASS=$OPTARG;;
- n) DBNAME=$OPTARG;;
+ u) USER=${OPTARG};;
+ p) PASS=${OPTARG};;
+ n) DBNAME=${OPTARG};;
  esac
 done
+
+DBTYPE_PG='pg'
+DBTYPE_MYSQL='mysql'
+if [ "$DBTYPE" != "$DBTYPE_PG" ] && [ "$DBTYPE" != "$DBTYPE_MYSQL" ]; then
+  echo Err: wrong DBTYPE must be 'pg' or 'mysql'
+  exit 1
+fi
 
 CSVPATH=./csv-data
 [ -d CSVPATH ] || mkdir -p $CSVPATH
@@ -38,12 +47,38 @@ rm -rf $CSVPATH/.csv
 # thus (bucket_name+path) will be enough to have precise location
 # aws s3 cp s3://dm-test-storage/tables /var/lib/mysql-files/ --recursive --exclude "*" --include "*.csv"
 aws s3 cp $S3URL $CSVPATH --recursive --exclude "*" --include "*.csv"
+
+if [ `ls -1U ./csv-data/*.csv | wc -l` -eq 0 ]; then
+    echo Err: CSV files not found at [$S3URL]
+    exit 1
+fi
 if [ $? -ne 0 ]; then exit $?; fi
 
-# upload to remote SERVER:PORT, using USER, PASS, DBNAME
+load2pg() {
+  psql -h $HOST -d $DBNAME -U $USER -c "\copy ${1%.*} ($2) from '${CSVPATH}/$1' with delimiter as ',' csv header"
+}
+
+load2mysql() {
+  mysqlimport -h $HOST -P $PORT --fields-terminated-by=, --ignore-lines=1 --columns=$2 --local -u $USER -p$PASS $DBNAME $CSVPATH/$1
+}
+
+if [ "$DBTYPE" == "$DBTYPE_PG" ]; then
+  export PGPASSWORD=$PASS
+fi
+
 # table name is implied from .csv file name, first line with col names is ignored
 for f in `ls ./csv-data`; do
   echo -e "\nImporting [${f%.*}]..."
-  psql -h $HOST -d $DBNAME -U $USER -c "\copy ${f%.*} from '${CSVPATH}/$f' with delimiter as ',' csv header"
+  COLUMNS=$(head -n 1 $CSVPATH/$f)
+  case "${DBTYPE}" in
+    ${DBTYPE_PG})
+      load2pg $f $COLUMNS
+      ;;
+    ${DBTYPE_MYSQL})
+      load2mysql $f $COLUMNS
+      ;;
+  esac
   echo -e "==> done"
 done
+
+echo -e "\nSUCCESS\n"
